a9f8e5a37d9445f4308d8c64d8bb32799c3ad3e1
Martyn Eggleton <martyn.eggleton@gmail.com>
2016-10-14 00:07:41 +0100

    vaguely working

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
-function GetGCSEStructure(Atomic, Mass) 
+var fs = require("fs");
+var Handlebars = require("handlebars");
 
-Var Structure = {protons: Atomic, 
+var aShellMaxes = [2,8,8,8] ;
+var aAngles = [0, 180, 90,  270, 45, 215, 135, 315];
 
-neutrons: Mass- Atomic, 
+var aElectonPoses = [];
+var i = 0 ;
+var iInnerOrbit = 3000;
 
-electrons: Atomic, 
 
-shells:[] 
+for(var j = 0; j<aShellMaxes.length; j++){
+  for(var k = 0; k<aShellMaxes[j]; k++){
+    aElectonPoses.push({
+      shell:j+1,
+      angle:aAngles[i%aAngles.length],
+      dist: j+1,
+      dur : (j+1)*iInnerOrbit
+    });
+    i++;
+  }
+}
 
-}  
+var source = fs.readFileSync("templates/atom.hbs", "utf8");
+var template = Handlebars.compile(source);
 
-Var Levels = [2,8,8,8] 
 
- 
+var sPT = fs.readFileSync("Periodic-Table-JSON/PeriodicTableJSON.json", "utf8");
+var oElements = JSON.parse(sPT);
 
-Var Remaining = Structure.electrons; 
+//console.log(oElements);
+for (sName in oElements){
+  var oElement = oElements[sName];
+  oElement.number = parseInt(oElement.number, 10);
 
-Levels.forEach{function(Max, Shell){ 
-
-Structure.shells[Shell] = Math.min(Remaining, Max) 
-
-Remaining -= Structure.shells[Shell]; 
-
-} 
-
-Return Structure ;  
-
-} 
+  if(oElement.number < 19){ // Less than Potassium
+    oElement.name = sName;
+    oElement.filename = "dist/"+sName+".html";
+    
+    oElement.protons = oElement.number;
+    oElement.neutrons = Math.round(oElement.atomic_mass) - oElement.number;
+    oElement.electrons = [];
+    for(i = 0; i<oElement.number; i++){
+      oElement.electrons.push(aElectonPoses[i]);
+    }
+    
+    var html = template(oElement);  
+    fs.writeFileSync(oElement.filename, html, "utf8");
+  }
+}
