5da25f77957aad8d97062e03d22095a38784978c
Martyn Eggleton <martyn.eggleton@gmail.com>
2016-10-15 17:26:57 +0100

    Tried a model of protons ans nuetrons not really worked

--- index.js
+++ index.js
@@ -32,6 +32,67 @@ var oElements = JSON.parse(sPT);
 
 var aAtoms = [];
 
+var aNucleusPositions = {1:  [{lat:0, lon:0, radius:0}]};
+  
+function getBallCount(Radius){
+  var MinAngleBetweenBalls = 2 * Math.atan(1/Radius);
+  var MaxBallsAroundLine = Math.floor( 2 * Math.PI / MinAngleBetweenBalls);
+  return MaxBallsAroundLine;
+}  
+
+function getAngleFromBalls(MaxBallsAroundLine){
+  var Angle = 2 * Math.PI / MaxBallsAroundLine;
+  return Angle;
+}  
+  
+function getAnglesAtRadius(Radius){
+  var MaxBallsAroundLine = getBallCount(Radius);
+  var Angle = getAngleFromBalls(MaxBallsAroundLine);
+  var Angles = [];
+  for (var i = 0 ; i < MaxBallsAroundLine; i++){
+    Angles.push(i * Angle);
+  }
+  return Angles;
+}
+
+function getNucleonPositions(Balls){
+  //https://en.wikipedia.org/wiki/Packing_problems#Spheres_into_a_Euclidean_ball
+  //var SmallestContainerRadius = 1 + Math.sqrt(2*(1-(1/Balls))); // Dosent seem to work
+  var SmallestContainerRadius = (Math.pow(Math.ceil(Balls/(Math.PI * (4/3))), 1/3));
+  
+  //V = ⁴⁄₃πr³.
+  
+  //var SmallestContainerRadius = Math.floor(Math.sqrt(Balls));
+  //var OuterBallRadius = (SmallestContainerRadius - 1);
+  var OuterBallRadius = SmallestContainerRadius;
+  
+  var MaxBallsAroundEquator = getBallCount(OuterBallRadius);
+  var Angle = getAngleFromBalls(MaxBallsAroundEquator);
+  
+  console.log("Balls", Balls, "SmallestContainerRadius",SmallestContainerRadius, "OuterBallRadius", OuterBallRadius,
+   "Equator", MaxBallsAroundEquator, "Angle", Angle, "Degrees", Angle * 180 / Math.PI);
+  
+  var RadialPositions = [];
+  var UpwardsLayers = Math.floor(0.5 + (MaxBallsAroundEquator/4));
+  console.log("UpwardsLayers", UpwardsLayers);
+  for(var q = 0; q < UpwardsLayers ; q++){
+    var UpAngle = q * Angle;
+    var RadiusAtHeight = OuterBallRadius * Math.cos(UpAngle);
+    var Angles = getAnglesAtRadius(RadiusAtHeight);
+    console.log("q,", q, "UpAngle", UpAngle * 180 / Math.PI, "RadiusAtHeight", RadiusAtHeight);//, "Angles", Angles);
+    for (var w = 0; w < Angles.length; w++){
+      RadialPositions.push({lat:UpAngle * 180 / Math.PI, lon:Angles[w] * 180 / Math.PI, radius:SmallestContainerRadius});
+      if(q > 0){
+        RadialPositions.push({lat:-1 * UpAngle * 180 / Math.PI, lon:Angles[w] * 180 / Math.PI, radius:SmallestContainerRadius});
+      }
+    }
+  }
+  
+  console.log("OuterShell", RadialPositions.length, RadialPositions.slice(0,6));
+  return RadialPositions;
+}
+
+
 //console.log(oElements);
 for (var sName in oElements){
   var oElement = oElements[sName];
@@ -40,13 +101,38 @@ for (var sName in oElements){
   if(oElement.number < 37){ // Less than Potassium
     oElement.name = sName;
     oElement.filename = "dist/"+sName+".html";
-    
+    oElement.nucleons = Math.round(oElement.atomic_mass);
     oElement.protons = oElement.number;
-    oElement.neutrons = Math.round(oElement.atomic_mass) - oElement.number;
+    oElement.neutrons =  oElement.nucleons - oElement.number;
     oElement.electrons = [];
+    oElement.nucleus = [];
     
+
+    //Periodic Table Layout    
     oElement.x = oElement.xpos * 5;
     oElement.y = (oElement.ypos * -5);
+    
+    /*if(aNucleusPositions.length == oElement.nucleons){
+      console.log("Creating Nucleon",aNucleusPositions.length == oElement.nucleons,aNucleusPositions.length, oElement.nucleons);
+      aNucleusPositions.push(getNucleonPositions(oElement.nucleons));
+    }*/
+    
+    var fProtonRadius = 0.2;
+    var aPositions = getNucleonPositions(oElement.nucleons).map(function(pos){
+        pos.radius = pos.radius*fProtonRadius;
+        pos.fProtonRadius = fProtonRadius;
+        return pos;
+      });
+    for (var i =0; i< Math.min(oElement.nucleons, aPositions.length); i++){
+      var oNew = aPositions[i];
+      oNew.type = (i%2 == 0)?"proton":"neutron";
+      oElement.nucleus.push(oNew);
+    }
+      console.log(oElement.name, oElement.nucleus.slice(0,-5));
+      
+      
+    
+    //electrons
     for(i = 0; i<oElement.number; i++){
       oElement.electrons.push(aElectonPoses[i]);
     }
@@ -58,6 +144,8 @@ for (var sName in oElements){
   }
 }
 
+var indexSource = fs.readFileSync("templates/index.hbs", "utf8");
+var indexTemplate = Handlebars.compile(indexSource);
 
 var TableSource = fs.readFileSync("templates/table.hbs", "utf8");
 var TableTemplate = Handlebars.compile(TableSource);
@@ -65,5 +153,10 @@ console.log(aAtoms.length);
 
 var sFilename = "dist/table.html";
 var html = TableTemplate({atoms:aAtoms});  
+fs.writeFileSync(sFilename, html, "utf8");
+console.log("Written", sFilename);
+
+var sFilename = "dist/index.html";
+var html = indexTemplate({atoms:aAtoms});  
 fs.writeFileSync(sFilename, html, "utf8");
 console.log("Written", sFilename);
